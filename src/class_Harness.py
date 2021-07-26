0660f849a074c30e78bf7b8d88db58e5c15a3680
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-07-26 16:53:26 -0700

    added time to Harness

--- src/class_Harness.py
+++ src/class_Harness.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 import numpy as np
 
@@ -10,16 +11,20 @@ class Harness:
     def __init__(self, scorer, random_state=2021):
         self.scorer = scorer
         self.history = pd.DataFrame(columns=['Name', 'F1', 'Recall',
-                                             'Precision', 'Notes'])
+                                             'Precision', 'CV_Time(sec)', 'Notes'])
 
     def report(self, model, X, y, name, notes='', cv=5,):
+        start_time = time.time()
         self.scores = cross_validate(model, X, y, 
                                  scoring=self.scorer, cv=cv)
+        end_time = time.time()
+        cv_time = end_time - start_time
         frame = pd.DataFrame([[name, self.scores['test_f1'].mean(),
                                self.scores['test_recall'].mean(),
-                               self.scores['test_precision'].mean(), notes]],
+                               self.scores['test_precision'].mean(),
+                               cv_time, notes]],
                              columns=['Name', 'F1', 'Recall',
-                                      'Precision', 'Notes'])
+                                      'Precision', 'CV_Time(sec)', 'Notes'])
         self.history = self.history.append(frame)
         self.history = self.history.reset_index(drop=True)
         self.history = self.history.sort_values('F1')
@@ -31,4 +36,3 @@ class Harness:
         print(f"{name} has an average F1 of {self.scores['test_f1'].mean()}")
         print(f"{name} has an average Recall of {self.scores['test_recall'].mean()}")
         print(f"{name} has an average Precision of {self.scores['test_precision'].mean()}")
-
