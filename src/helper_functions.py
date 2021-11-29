812c3bb781c9909b9f2412cfce599db041f1dde1
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-11-29 12:37:08 -0800

    Refactors helper_functions.py imports

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,21 +1,20 @@
 # importing 
-import os, sys
+import os
 import pickle
+import sys
 
-import pandas as pd
+import dataframe_image as dfi
+import matplotlib.image as img
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
-import statsmodels.api as sm
-
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (accuracy_score, f1_score, recall_score, precision_score,
                              make_scorer, plot_confusion_matrix, confusion_matrix, roc_auc_score)
 from sklearn.inspection import permutation_importance
-
-import matplotlib.pyplot as plt
-import matplotlib.image as img
-import dataframe_image as dfi
-import seaborn as sns
+import statsmodels.api as sm
 
 # setting project path
 gparent = os.path.join(os.pardir, os.pardir)
